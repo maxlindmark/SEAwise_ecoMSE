155a25cb1389e99c34f1f9f233a7657cf251d754
Marc Taylor <marchtaylor@gmail.com>
2024-11-20 10:56:11 +0100

    del

--- render_Rmarkdowns.R
+++ render_Rmarkdowns.R
@@ -8,4 +8,4 @@ rmarkdown::render(
 )
 
 # extract code
-knitr::purl(input = "modelSelection/modelSelection.Rmd", output = "modelSelection.R", documentation = 1)
+knitr::purl(input = "modelSelection/modelSelection.Rmd", output = "modelSelection/modelSelection.R", documentation = 1)
